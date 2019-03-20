0a22bc84b91ced2785992ec22ddcc40e1fa58b46
michal.marszalek <michal.marszalek@pl.abb.com>
2019-03-20 18:01:37 +0100

    comment addded

--- AzureFunction.DI/IModule.cs
+++ AzureFunction.DI/IModule.cs
@@ -2,6 +2,9 @@ using Microsoft.Extensions.DependencyInjection;
 
 namespace AzureFunction.DI
 {
+    /// <summary>
+    /// 
+    /// </summary>
     public interface IModule
     {
         void Load(IServiceCollection services);
